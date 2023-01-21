abb3b5792ed84893624ec5403379f907051628d1
rlagkswn00 <hjkim2902@naver.com>
2023-01-21 23:51:36 +0900

    회원 기능 테스트

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -1,13 +1,40 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
 public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
 
     @Test
     public void 회원가입() throws Exception{
+        //when
+        Member member = new Member();
+        member.setName("kim");
+
+        //given
+        Long saveId = memberService.join(member);
+
+        //then
+        //결과가 같은지 확인
+        assertEquals(member,memberRepository.findOne(saveId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception{
 
     }
 }
