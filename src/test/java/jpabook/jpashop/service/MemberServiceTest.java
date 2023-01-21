1a35a8bb5daabfcbf22a645da09bf3d999b14069
rlagkswn00 <hjkim2902@naver.com>
2023-01-21 23:46:56 +0900

    회원 기능 테스트

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MemberServiceTest {
+
+    @Test
+    public void 회원가입() throws Exception{
+
+    }
+}
