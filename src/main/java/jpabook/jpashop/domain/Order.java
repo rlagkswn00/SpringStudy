97683f8224cb4d0ea96d57c97c84ce1ccdce6b0a
rlagkswn00 <hjkim2902@naver.com>
2023-02-20 18:30:27 +0900

    주문, 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -60,6 +60,8 @@ public class Order {
         delivery.setOrder(this);
     }
 
+
+    //==생성 메서드==//
     public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
         Order order = new Order();
         order.setMember(member);
@@ -70,5 +72,16 @@ public class Order {
         order.setStatus(OrderStatus.ORDER);
         order.setOrderDate(LocalDateTime.now());
         return order;
+    }
+
+    //==주문 취소==//
+    public void cancel(){
+        if(delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem orderItem : this.orderItems){
+            orderItem.cancel();
+        }
     }
 }
