822f69b6fa5e2f62713be93244130a7e2ffa2ac3
rlagkswn00 <hjkim2902@naver.com>
2023-03-16 15:26:42 +0900

    싱글톤 컨테이너의 문제점

--- core/src/test/java/hello/core/singleton/SingleTonTest.java
+++ core/src/test/java/hello/core/singleton/SingleTonTest.java
@@ -4,6 +4,8 @@ import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.testng.annotations.Test;
 
 public class SingleTonTest {
@@ -11,14 +13,16 @@ public class SingleTonTest {
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
     void pureContainer() {
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
 
-        MemberService memberService1 = appConfig.memberService();
-
-        MemberService memberService2 = appConfig.memberService();
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
 
         System.out.println("memberService1 = " + memberService1);
         System.out.println("memberService2 = " + memberService2);
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
     }
 
     @Test
