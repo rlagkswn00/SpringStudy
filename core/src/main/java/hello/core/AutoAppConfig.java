d0c1fc9d45c6e3598314f3eda4cf6dd382273daa
rlagkswn00 <hjkim2902@naver.com>
2023-03-18 11:09:58 +0900

    컴포넌트 필터 옵션

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        basePackages = "hello.core.member",
+        excludeFilters= @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+
 )
 public class AutoAppConfig {
 
--- core/src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -1,5 +1,6 @@
-package hello.core;
+package hello.core.scan;
 
+import hello.core.AutoAppConfig;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
