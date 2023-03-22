336edd7b3e622cfb2653d84d2c56a6d65945a7ed
rlagkswn00 <hjkim2902@naver.com>
2023-03-22 20:12:28 +0900

    싱글톤, 프로토타입 빈 공유시 문제

---
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,78 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import java.lang.annotation.Annotation;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonWithPrototypeTest1 {
+
+    @Test
+    void prototypeFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean bean = ac.getBean(PrototypeBean.class);
+        bean.addCount();
+        assertThat(bean.getCount()).isEqualTo(1);
+
+        PrototypeBean bean2 = ac.getBean(PrototypeBean.class);
+        bean2.addCount();
+        assertThat(bean2.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    void singletonClientUsePrototype(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+        ClientBean bean = ac.getBean(ClientBean.class);
+        int count1 = bean.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean bean2 = ac.getBean(ClientBean.class);
+        int count2 = bean2.logic();
+        assertThat(count2).isEqualTo(2);
+    }
+
+    @Scope("singleton")
+    static class ClientBean{
+        private final PrototypeBean prototypeBean;
+
+        @Autowired
+        public ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic(){
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean{
+        private int count =0;
+
+        public void addCount(){
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init " + this);
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("PrototypeBean.destroy " + this);
+        }
+    }
+}
