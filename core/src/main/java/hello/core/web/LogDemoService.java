b581b81557ba58c1ed6c41efba164ffc33ba4de3
rlagkswn00 <hjkim2902@naver.com>
2023-03-22 20:54:06 +0900

    reqeust scope with Provider

--- core/src/main/java/hello/core/web/LogDemoService.java
+++ core/src/main/java/hello/core/web/LogDemoService.java
@@ -1,6 +1,7 @@
 package hello.core.web;
 
 import hello.core.common.MyLogger;
+import jakarta.inject.Provider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,8 +9,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    public final MyLogger myLogger;
+    public final Provider<MyLogger> myLoggerProvider;
     public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.get();
         myLogger.log("service id = " + id);
     }
 }
