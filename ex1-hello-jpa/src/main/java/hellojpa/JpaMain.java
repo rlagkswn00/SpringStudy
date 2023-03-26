25aca64b117fdc666f04b169d9f6cf5a6a0bd794
rlagkswn00 <hjkim2902@naver.com>
2023-03-26 23:06:25 +0900

    영속성 컨테스트

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -14,13 +14,27 @@ public class JpaMain {
         tx.begin();
         try{
             //비영속
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA");
 
             //영속 상태
-            em.persist(member);
+//            em.persist(member);
+//            Member findMember = em.find(Member.class, 101L);
+//
+//            System.out.println("findMember = " + findMember);
+//            em.find(Member.class, 101L);
+//            Member findMember2 = em.find(Member.class, 101L);
+//
+//            System.out.println("findMember2 = " + findMember2);
+//
+//            System.out.println("result= " + (findMember == findMember2));
+            Member member = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
 
+            em.persist(member2);
+            em.persist(member);
+            System.out.println("===============+");
             tx.commit();
         }catch (Exception e){
             tx.rollback();
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -9,6 +9,11 @@ public class Member {
     private Long id;
     private String name;
 
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
