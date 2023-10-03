274a600c08b176c61935c31afb23ea13497b7e35
Han Ju Kim <hjkim2902@gmail.com>
2023-10-03 20:20:39 +0900

    add : MVC1 프로젝ㅌ 생성

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,8 @@
 package hellojpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 public class JpaMain {
@@ -16,13 +15,16 @@ public class JpaMain {
         try{
             Member member = new Member();
             member.setName("user");
-            member.setCreatedBy("222");
-            member.setCreatedDate(LocalDateTime.now());
 
             em.persist(member);
 
             em.flush();
             em.clear();
+            Member findMember = em.getReference(Member.class, member.getId());
+
+            System.out.println("isLoaded: " + emf.getPersistenceUnitUtil().isLoaded(findMember));
+            System.out.println("findMember.getName() = " + findMember.getName());
+            Hibernate.initialize(findMember);
 
             tx.commit();
         }catch (Exception e){
