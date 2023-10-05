3f8765d5a9f4fc663a835a0faf307a6a46c02121
Han Ju Kim <hjkim2902@gmail.com>
2023-10-05 23:32:33 +0900

    서블릿 request 메소드 실행

---
+++ servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,31 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
+public class RequestHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        System.out.println("--- REQUEST-LINE - start ---");
+        System.out.println("request.getMethod() = " + req.getMethod()); //GET
+        System.out.println("request.getProtocol() = " + req.getProtocol()); // HTTP/1.1
+        System.out.println("request.getScheme() = " + req.getScheme()); //http
+        // http://localhost:8080/request-header
+        System.out.println("request.getRequestURL() = " + req.getRequestURL());
+        // /request-header
+        System.out.println("request.getRequestURI() = " + req.getRequestURI());
+        //username=hi
+        System.out.println("request.getQueryString() = " +
+                req.getQueryString());
+        System.out.println("request.isSecure() = " + req.isSecure()); //https 사용 유무
+        System.out.println("--- REQUEST-LINE - end ---");
+        System.out.println();
+    }
+}
