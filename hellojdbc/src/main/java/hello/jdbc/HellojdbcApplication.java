89e759c2bb8183f29f7058adf1a887a8d3d105e9
Han Ju Kim <hjkim2902@gmail.com>
2023-10-27 23:13:38 +0900

    init commit

---
+++ hellojdbc/src/main/java/hello/jdbc/HellojdbcApplication.java
@@ -0,0 +1,13 @@
+package hello.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+    @SpringBootApplication
+    public class HellojdbcApplication {
+
+        public static void main(String[] args) {
+        SpringApplication.run(HellojdbcApplication.class, args);
+    }
+
+}
